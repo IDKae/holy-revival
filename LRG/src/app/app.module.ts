56c2fa0701173e15804874b209b5e3c72baab49e
Rhylen 'Jennifer' Nguyen <rhylennguyen@gmail.com>
2022-07-06 15:53:28 -0400

    Add Express Server

--- LRG/src/app/app.module.ts
+++ LRG/src/app/app.module.ts
@@ -1,14 +1,18 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpModule } from '@angular/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
